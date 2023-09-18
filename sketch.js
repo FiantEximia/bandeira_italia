c2cb3969a8a8ef0b23587ed78c8e9d7389a4d718
FiantEximia <137070599+FiantEximia@users.noreply.github.com>
2023-09-17 21:23:32 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 300);
+  }
+  
+  function draw() {
+    background("white");
+    fill("green")
+    rect(0,0,200,300);
+    fill("white");
+    rect(200,0,200,300);
+    fill("red");
+    rect(400,0,200,300)
+  }
